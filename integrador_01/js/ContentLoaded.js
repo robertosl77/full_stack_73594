9b3c0b9bc0386e4d522f2a3dd751421f50188a89
robertosl77 <robertosl77@gmail.com>
2025-02-21 17:40:12 -0300

    integrador_01_v1.1
    se resuelve la importacion de componentes html con iframe ya que al ser codigo nativo el DOM no funciona correctamente.

--- integrador_01/js/ContentLoaded.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("header.html")
-        .then(response => response.text())
-        .then(data => document.getElementById("header-container").innerHTML = data);
-
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(data => document.getElementById("footer-container").innerHTML = data);
-});
