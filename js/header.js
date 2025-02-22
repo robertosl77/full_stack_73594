bcf5bfd91d61b7d0d6b3c554ba10681307992bb3
robertosl77 <robertosl77@gmail.com>
2025-02-21 21:14:36 -0300

    integrador_01_v2
    abre el diseño de productos en la pagina de inicio

--- js/header.js
+++ js/header.js
@@ -2,16 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const header = document.createElement("header");
     header.innerHTML = `
         <nav>
-            <a href="index.html"><img src="logo.png" alt="Logo"></a>
+            <a href="index.html"><img class="logo" src="img/logo.png" alt="Logo"></a>
             <ul>
                 <li><a href="index.html">Inicio</a></li>
                 <li><a href="alta.html">Alta de Productos</a></li>
                 <li><a href="contacto.html">Contacto</a></li>
                 <li><a href="nosotros.html">Nosotros</a></li>
+                <li><a href="#" id="login-button">Iniciar sesión</a></li>
             </ul>
-            <div id="user-section">
-                <button id="login-button">Iniciar sesión</button>
-            </div>
         </nav>
     `;
     document.body.insertBefore(header, document.body.firstChild);
