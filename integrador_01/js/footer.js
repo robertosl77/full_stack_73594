2b4150d1372f7fa000591d4d4bf3585c77754e7c
robertosl77 <robertosl77@gmail.com>
2025-02-23 20:35:51 -0300

    integrador_01_v3.10

--- integrador_01/js/footer.js
+++ integrador_01/js/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const footer = document.createElement("footer");
-    footer.innerHTML = `<p>&copy; 2025 - Todos los derechos reservados</p>`;
+    footer.innerHTML = `<p>&copy; 2025 realizado por Roberto Sanchez Leiva</p>`;
     document.body.appendChild(footer);
 });
