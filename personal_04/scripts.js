4c520dddeb0cdc6afe8887631ff4ca7711e2d311
robertosl77 <robertosl77@gmail.com>
2025-03-18 21:41:40 -0300

    actividad_06_grupal_v1

--- personal_04/scripts.js
+++ personal_04/scripts.js
@@ -1,10 +1,4 @@
 const f = document.createDocumentFragment();
-const names = ['susana', 'andrea', 'lorena'];
-
-names.forEach(n => {
-    const div = document.createElement("div");
-    div.textContent = n;
-    f.appendChild(div);
-});
-
+const names = ["susana", "andrea", "lorena"]; // Corregido el formato de las comillas
+names.forEach(n => f.innerHTML += `<div>${n}</div>`); // Esta línea sigue sin funcionar correctamente
 document.getElementById('app').appendChild(f);
