bebc814bcb987a97338a9812879b20e6a5c84489
robertosl77 <robertosl77@gmail.com>
2025-03-24 19:53:28 -0300

    actividad_06_grupal_v1.6

---
+++ actividad-06-grupal/js/estilos-ciclo.js
@@ -0,0 +1,33 @@
+const estilos = [
+    "estilo-base",
+    "estilo-retro",
+    "estilo-futuro",
+    "estilo-propio1"
+];
+
+let indiceEstilo = 0;
+
+window.addEventListener("load", () => {
+    const btn = document.createElement("button");
+    btn.id = "cambiarEstilo";
+    btn.textContent = "Cambiar Estilo";
+    btn.style.position = "fixed";
+    btn.style.top = "10px";
+    btn.style.right = "10px";
+    btn.style.width = "180px";
+    btn.style.zIndex = "9999";
+    btn.className="btn";
+    
+    document.body.prepend(btn);
+
+    btn.addEventListener("click", () => {
+        // Desactivar todos
+        estilos.forEach(id => {
+            document.getElementById(id).disabled = true;
+        });
+
+        // Activar el siguiente
+        indiceEstilo = (indiceEstilo + 1) % estilos.length;
+        document.getElementById(estilos[indiceEstilo]).disabled = false;
+    });
+});
---
+++ actividad-06-grupal/js/estilos-random.js
@@ -0,0 +1,27 @@
+// La variable estilos viene de estilos-ciclo.js
+
+window.addEventListener("load", () => {
+    const btn = document.createElement("button");
+    btn.id = "cambiarRandom";
+    btn.textContent = "Cambiar Random";
+    btn.style.position = "fixed";
+    btn.style.top = "60px";
+    btn.style.right = "10px";
+    btn.style.width = "180px";
+    btn.style.zIndex = "9999";
+    btn.className="btn";
+    
+    document.body.prepend(btn);
+
+    btn.addEventListener("click", () => {
+        // Desactivar todos
+        estilos.forEach(id => {
+            document.getElementById(id).disabled = true;
+        });
+
+        // Activar el siguiente
+        let random = Math.floor(Math.random() * estilos.length);
+        console.log("aleatorio: "+random);
+        document.getElementById(estilos[random]).disabled = false;
+    });
+});
