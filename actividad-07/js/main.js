20c9a3b2f13b922c448a8bd8328da3351fe53fde
rsleiva <rsleiva@edenor.com>
2025-03-31 11:22:39 -0300

    actividad_07_v2
    botones

--- actividad-07/js/main.js
+++ actividad-07/js/main.js
@@ -1,3 +1,16 @@
+function change_disabled(inputs, botones, estado){
+    // Cambia el estado de los inputs
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = estado;
+    }
+    // Cambia el estado de los botones
+    for (let i = 0; i < botones.length; i++) {
+        botones[i].disabled = estado;
+    }
+    // Cambia el estado del div
+    document.getElementById('destino').innerHTML = '';
+}
+
 window.addEventListener('load', function() {
     // Ejercicio punto 2
     console.log('Contenido del DOM cargado');
@@ -6,34 +19,28 @@ window.addEventListener('load', function() {
     const html = '<p>Este contenido <strong>está listo</strong><br>para ser editado y pasarlo abajo.</p>';
     const textarea = document.getElementById('origen');
     textarea.value = html;
-
+    
     // Ejercicio punto 4
-    if (textarea.value.trim() !== '') {
-        // Habilitar botones al cargar si hay contenido
-        const inputs = document.getElementsByTagName('input');
-        const botones = document.getElementsByTagName('button');
+    const inputs = document.getElementsByTagName('input');
+    const botones = document.getElementsByTagName('button');
+    const habilitar = textarea.value.trim() !== '';
 
-        for (let i = 0; i < inputs.length; i++) {
-        inputs[i].disabled = false;
-        }
+    change_disabled(inputs,botones, !habilitar);
 
-        for (let i = 0; i < botones.length; i++) {
-        botones[i].disabled = false;
-        }
-    }
-
-    // También al escribir
     textarea.addEventListener('input', function() {
-        const inputs = document.getElementsByTagName('input');
-        const botones = document.getElementsByTagName('button');
+        const habilitar = textarea.value.trim() !== '';
+        change_disabled(inputs,botones, !habilitar);
+    });    
 
-        for (let i = 0; i < inputs.length; i++) {
-        inputs[i].disabled = false;
-        }
+    // Ejercicio punto 5 - Botón Reemplazar
+    document.getElementById('btn-reemplazar').addEventListener('click', function() {
+        document.getElementById('destino').innerHTML = textarea.value;
+    });
 
-        for (let i = 0; i < botones.length; i++) {
-        botones[i].disabled = false;
-        }
+    // Ejercicio punto 6 - Botón Agregar
+    const btnAgregar = document.getElementsByClassName('btn-agregar')[0];
+    btnAgregar.addEventListener('click', function() {
+        document.getElementById('destino').innerHTML += textarea.value;
     });
 
 });
