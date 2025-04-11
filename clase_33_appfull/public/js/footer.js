7be23d0396fc726a8d98e91bc22d27402f83b23a
robertosl77 <robertosl77@gmail.com>
2025-04-11 19:48:12 -0300

    clase_33_backfrontinside

---
+++ clase_33_appfull/public/js/footer.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const footer = document.createElement("footer");
+    footer.innerHTML = `<p>&copy; 2025 realizado por Roberto Sanchez Leiva</p>`;
+    document.body.appendChild(footer);
+});
