fc4f3be2c3730739e1bc3015598fa30a4e060207
robertosl77 <robertosl77@gmail.com>
2025-04-27 20:49:44 -0300

    actividad_09_v5.5
    editar contacto

--- actividad_09/public/index.js
+++ actividad_09/public/index.js
@@ -94,7 +94,7 @@ async function cargarContactos() {
             <div class="col-md-3">${email}</div>
             <div class="col-md-2">${formatearFecha(fechaNacimiento)}</div>
             <div class="col-md-4 text-end">
-              <button class="btn btn-sm btn-warning me-2">
+              <button class="btn btn-sm btn-warning me-2 btn-editar" data-id="${_id}" data-nombre="${nombre}" data-email="${email}" data-fecha="${fechaNacimiento}">
                 <i class="bi bi-pencil-fill"></i>
               </button>
               <button class="btn btn-sm btn-danger btn-eliminar" data-id="${_id}">
@@ -106,10 +106,10 @@ async function cargarContactos() {
   
         divContactos.appendChild(fila);
       });
-      
-      localStorage.setItem('contactos', JSON.stringify(contactos));
 
-      agregarEventosEliminar(); // importante después de crear la lista
+      localStorage.setItem('contactos', JSON.stringify(contactos));
+      agregarEventosEliminar();
+      agregarEventosEditar();
   
     } catch (error) {
       console.error('Error al cargar contactos:', error);
@@ -170,5 +170,27 @@ function filtrarContactos(termino) {
       } else {
         fila.style.display = 'none';
       }
+    });
+}  
+
+function agregarEventosEditar() {
+    const botonesEditar = document.querySelectorAll('.btn-editar');
+  
+    botonesEditar.forEach(boton => {
+      boton.addEventListener('click', (e) => {
+        const id = boton.getAttribute('data-id');
+        const nombre = boton.getAttribute('data-nombre');
+        const email = boton.getAttribute('data-email');
+        const fechaNacimiento = boton.getAttribute('data-fecha').slice(0, 10); // Ajuste formato
+  
+        // Cargar valores en el form de editar
+        document.getElementById('editarId').value = id;
+        document.getElementById('editarNombre').value = nombre;
+        document.getElementById('editarEmail').value = email;
+        document.getElementById('editarFechaNacimiento').value = fechaNacimiento;
+  
+        // Mostrar el collapse de edición
+        const collapseEditar = new bootstrap.Collapse('#editar_contactos', { toggle: true });
+      });
     });
 }  
