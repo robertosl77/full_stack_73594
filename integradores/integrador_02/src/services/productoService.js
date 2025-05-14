f23df91ef91997192a156bd58726b663b6895c51
robertosl77 <robertosl77@gmail.com>
2025-05-13 22:39:19 -0300

    integrador_02_v2.3

--- integradores/integrador_02/src/services/productoService.js
+++ integradores/integrador_02/src/services/productoService.js
@@ -2,10 +2,20 @@ import Producto from '../models/producto.js';
 
 export async function obtenerProductosConDescuento() {
     const productos = await Producto.find();
-    return productos.map(p => ({
-        ...p.toObject(),
-        precioDescuento: p.descuento > 0 
-            ? Math.round(p.precio_original * (1 - p.descuento / 100)) 
-            : p.precio_original
-    }));
+    return productos.map(p => {
+        const precioDescuentoValor = p.descuento > 0 
+            ? p.precio_original * (1 - p.descuento / 100) 
+            : p.precio_original;
+
+        const formatoPrecio = valor => new Intl.NumberFormat('es-AR', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }).format(valor);
+
+        return {
+            ...p.toObject(),
+            precioDescuento: formatoPrecio(precioDescuentoValor),
+            precioOriginalFormateado: formatoPrecio(p.precio_original)
+        };
+    });
 }
