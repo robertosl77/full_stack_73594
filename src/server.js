0128fb522baab171b8cb30f5bde929a83661f3d6
robertosl77 <robertosl77@gmail.com>
2025-06-03 19:17:47 -0300

    integrador2_beta_v1.1

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
     console.info(`Servidor corriendo en http://localhost:${PORT}${process.env.BASEDIR}`);
