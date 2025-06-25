564fb4534a81e4e83877aa90b9a865b4e277efc7
robertosl77 <robertosl77@gmail.com>
2025-06-25 19:12:41 -0300

    clase_48_react

---
+++ clases/clase_48_react/app001/src/Componentes/Navegacion/Navegacion.js
@@ -0,0 +1,13 @@
+const Navegacion = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/productos">Productos</a></li>
+        <li><a href="/contacto">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navegacion;
