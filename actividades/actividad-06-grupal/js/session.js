8a7394a8a1ffdfbf075b0c0295701895dafc0e06
robertosl77 <robertosl77@gmail.com>
2025-07-22 23:12:15 -0300

    conf

--- actividades/actividad-06-grupal/js/session.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-    if (!sessionStorage.getItem("usuarios")) {
-        const datos = obtenerDatos();
-        sessionStorage.setItem("usuarios", JSON.stringify(datos));
-    }
-
-    console.log(JSON.parse(sessionStorage.getItem("usuarios")));
-});
